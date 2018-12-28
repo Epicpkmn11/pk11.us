c0fac271ff1cddfcc1ac0cd7ae469a527687c3c0
Epicpkmn11 <epicpkmn11@outlook.com>
2018-12-28 00:01:29 -0600

    Add some more splashes
    b/c I'm bored ;P

--- js/splashes.js
+++ js/splashes.js
@@ -5,7 +5,11 @@ $(document).ready(function () {
 function RandomSplash() {
 	splashes = [
 	"That one guy who made the HBChecker script",
-	"<span style=\"color:#900;\">Bumper</span> <span style=\"color:#00c;\">Boys</span> <span style=\"color:#900;\">with</span> <span style=\"color:#00c;\">a</span> <span style=\"color:#900;\">Z</span><span style=\"color:#00c;\">!</span>"
+	"<span style=\"color:#900;\">Bumper</span> <span style=\"color:#00c;\">Boys</span> <span style=\"color:#900;\">with</span> <span style=\"color:#00c;\">a</span> <span style=\"color:#900;\">Z</span><span style=\"color:#00c;\">!</span>",
+	"<em>#DSiBestConsole!</em>",
+	"Anyone else still use <span style=\"font-size:80%;\">ニンテンドー</span>DSi<span style=\"font-size:80%;\">サウンド</span>?",
+	"<a href=\"https://twitter.com/@ComoBEASTBot\">@ComoBEASTBot</a> ← The <em>coolest</em> Twitter ;P",
+	".o/"
 	]
-	document.getElementById('splash').innerHTML = splashes[Math.floor(Math.random() * 2)];
+	document.getElementById('splash').innerHTML = splashes[Math.floor(Math.random() * 6)];
 }
