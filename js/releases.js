52c54dc342b241f042be19991d09ab29eca6b355
Epicpkmn11 <epicpkmn11@outlook.com>
2018-12-28 01:16:11 -0600

    Switch " to ' in js

--- js/releases.js
+++ js/releases.js
@@ -3,30 +3,30 @@ $(document).ready(function () {
 });
 
 function GetLatestReleaseInfo() {
-	$.getJSON("https://api.github.com/repos/epicpkmn11/HBChecker/tags").done(function (json) {
+	$.getJSON('https://api.github.com/repos/epicpkmn11/HBChecker/tags').done(function (json) {
 	var release = json[0];
-	var verPosStrt = release.zipball_url.lastIndexOf("v");
+	var verPosStrt = release.zipball_url.lastIndexOf('v');
 	var verPosEnd = release.zipball_url.length-1;
 	var version = release.zipball_url.substr(verPosStrt,verPosEnd);
-	exeURL = "https://github.com/Epicpkmn11/HBChecker/releases/download/"+version+"/HBChecker.exe"
-	$("#exeDownload").attr("href", exeURL);
-	pyURL = "https://github.com/Epicpkmn11/HBChecker/releases/download/"+version+"/HBChecker.py"
-	$("#pyDownload").attr("href", pyURL);
-	jsonURL = "https://github.com/Epicpkmn11/HBChecker/releases/download/"+version+"/HBCheckerItems.json"
-	$("#jsonDownload").attr("href", jsonURL);
-	altJsonURL = "https://github.com/Epicpkmn11/HBChecker/releases/download/"+version+"/AltHBCheckerItems.zip"
-	$("#altJsonDownload").attr("href", altJsonURL);
+	exeURL = 'https://github.com/Epicpkmn11/HBChecker/releases/download/'+version+'/HBChecker.exe'
+	$('#exeDownload').attr('href', exeURL);
+	pyURL = 'https://github.com/Epicpkmn11/HBChecker/releases/download/'+version+'/HBChecker.py'
+	$('#pyDownload').attr('href', pyURL);
+	jsonURL = 'https://github.com/Epicpkmn11/HBChecker/releases/download/'+version+'/HBCheckerItems.json'
+	$('#jsonDownload').attr('href', jsonURL);
+	altJsonURL = 'https://github.com/Epicpkmn11/HBChecker/releases/download/'+version+'/AltHBCheckerItems.zip'
+	$('#altJsonDownload').attr('href', altJsonURL);
 	document.getElementById('relNum').innerHTML = version;
 	});
 
-	$.getJSON("https://api.github.com/repos/RocketRobz/TWiLightMenu/tags").done(function (json) {
+	$.getJSON('https://api.github.com/repos/RocketRobz/TWiLightMenu/tags').done(function (json) {
 	release = json[0];
-	verPosStrt = release.zipball_url.lastIndexOf("v")+1;
+	verPosStrt = release.zipball_url.lastIndexOf('v')+1;
 	verPosEnd = release.zipball_url.length-1;
 	var versionNew = release.zipball_url.substr(verPosStrt,verPosEnd);
 	var versionOld = document.getElementById('twlVer').innerHTML;
 	if(versionNew != versionOld){
-		document.getElementById('twlNotice').innerHTML += "<span style=\"font-size: 60%;\"></code>(Note: Newer release of TWiLight Menu++ was detected ("+versionNew+"), you may get corruption errors.)";	
+		document.getElementById('twlNotice').innerHTML += '<span style="font-size: 60%;"></code>(Note: Newer release of TWiLight Menu++ was detected ('+versionNew+'), you may get corruption errors.)';	
 	}
 
 	});
--- js/splashes.js
+++ js/splashes.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
 
 function RandomSplash() {
 	splashes = [
-	"That one guy who made the HBChecker script",
-	"<span style=\"color:#900;\">Bumper</span> <span style=\"color:#00c;\">Boys</span> <span style=\"color:#900;\">with</span> <span style=\"color:#00c;\">a</span> <span style=\"color:#900;\">Z</span><span style=\"color:#00c;\">!</span>",
-	"<em>#DSiBestConsole!</em>",
-	"Anyone else still use <span style=\"font-size:80%;\">ニンテンドー</span>DSi<span style=\"font-size:80%;\">サウンド</span>?",
-	"<a href=\"https://twitter.com/@ComoBEASTBot\">@ComoBEASTBot</a> ← The <em>coolest</em> Twitter ;P",
-	".o/",
-	"30 Pin > Lightning",
-	"I still use an iPhone 4S ;P <span style=\"font-size:80%\">(And an iPad 2...</span> <span style=\"font-size:70%\">And a DSi... </span><span style=\"font-size:60%\">And an iPod... </span><span style=\"font-size:50%\">Is it 2011?)</span>"
+	'That one guy who made the HBChecker script',
+	'<span style="color:#900;">Bumper</span> <span style="color:#00c;">Boys</span> <span style="color:#900;">with</span> <span style="color:#00c;">a</span> <span style="color:#900;">Z</span><span style="color:#00c;">!</span>',
+	'<em>#DSiBestConsole!</em>',
+	'Anyone else still use <span style="font-size:80%;">ニンテンドー</span>DSi<span style="font-size:80%;">サウンド</span>?',
+	'<a href="https://twitter.com/@ComoBEASTBot">@ComoBEASTBot</a> ← The <em>coolest</em> Twitter ;P',
+	'.o/',
+	'30 Pin > Lightning',
+	'I still use an iPhone 4S ;P <span style="font-size:80%">(And an iPad 2...</span> <span style="font-size:70%">And a DSi... </span><span style="font-size:60%">And an iPod... </span><span style="font-size:50%">Is it 2011?)</span>'
 	]
 	document.getElementById('splash').innerHTML = splashes[Math.floor(Math.random() * 8)];
 }
