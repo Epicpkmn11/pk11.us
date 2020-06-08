53a14fe8f4d78a46b603982cdf19fc2c7bf359c3
Pk11 <epicpkmn11@outlook.com>
2020-06-07 21:17:51 -0500

    Change theming to use CSS variables

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,15 +1,11 @@
-if(localStorage["theme"] == "light") {
-	document.getElementById("theme").href = "";
-} else if(localStorage["theme"] == "dark") {
-	document.getElementById("theme").href = "/assets/css/dark.css";
-}
+document.getElementById("theme").href = `/assets/css/${localStorage.theme}.css`;
 
 function changeTheme() {
-	if(localStorage["theme"] == "dark") { // Change to light
-		document.getElementById("theme").href = "";
-		localStorage["theme"] = "light";
-	} else { // Change to dark
-		document.getElementById("theme").href = "/assets/css/dark.css";
-		localStorage["theme"] = "dark";
-	}
+	if(localStorage["theme"] == "dark") // Change to light
+		localStorage.theme = "light";
+	else // Change to dark
+		localStorage.theme = "dark";
+
+	// Update active stylesheet
+	document.getElementById("theme").href = `/assets/css/${localStorage.theme}.css`;
 }
