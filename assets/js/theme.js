0c3e07fbdd478cf52ed46b82822ba7b3aa45d471
Pk11 <epicpkmn11@outlook.com>
2020-06-07 20:14:03 -0500

    Redo theme.js a bit

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,23 +1,15 @@
-if(getTheme() == 'light') {
-	document.getElementById('theme').href = '';
-} else if(getTheme() == 'dark') {
-	document.getElementById('theme').href = '/assets/css/dark.css';
-}
-
-function getTheme() {
-	var theme = localStorage.getItem('theme');
-	return theme;
-}
-function setTheme(theme) {
-	localStorage.setItem('theme', theme);
+if(localStorage.getItem("theme") == "light") {
+	document.getElementById("theme").href = "";
+} else if(localStorage.getItem("theme") == "dark") {
+	document.getElementById("theme").href = "/assets/css/dark.css";
 }
 
 function changeTheme() {
-	if(getTheme() == 'dark') {
-		document.getElementById('theme').href = '';
-		setTheme('light');
-	} else {
-		document.getElementById('theme').href = '/assets/css/dark.css';
-		setTheme('dark');
+	if(localStorage.getItem("theme") == "dark") { // Change to light
+		document.getElementById("theme").href = "";
+		localStorage.setItem("theme", "light")
+	} else { // Change to dark
+		document.getElementById("theme").href = "/assets/css/dark.css";
+		localStorage.setItem("theme", "dark")
 	}
 }
