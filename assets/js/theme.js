0a3e129bb8149ecd3a3ecf8a89551f7f4c4b7839
Pk11 <epicpkmn11@outlook.com>
2020-06-09 02:20:24 -0500

    Derp fix

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,7 +2,7 @@ if(localStorage.theme)
 	document.getElementById("theme").href = `/assets/css/${localStorage.theme}.css`;
 
 function changeTheme() {
-	if(localStorage.theme. == "dark") // Change to light
+	if(localStorage.theme == "dark") // Change to light
 		localStorage.theme = "light";
 	else // Change to dark
 		localStorage.theme = "dark";
