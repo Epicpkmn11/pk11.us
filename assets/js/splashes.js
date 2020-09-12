c141e2e5fdd764dcae0a8045211702f146bb58de
Pk11 <epicpkmn11@outlook.com>
2020-09-11 21:17:11 -0500

    Add some more splash text

--- assets/js/splashes.js
+++ assets/js/splashes.js
@@ -1,9 +1,27 @@
+if(!localStorage.visitCount)
+	localStorage.visitCount = 0;
+localStorage.visitCount++;
+
+const eyes = ['O', 'o', 'U', 'u', '>', '^', '-', 'X', 'T', 'q'];
+const mouths = ['w', 'u', 'o', '\\_', '-', 'x', '///'];
+const extras = [['', ''], ['', ''], ['', ''], ['', '-☆'], ['=', '='], ['d', 'b♪']];
+
+function face() {
+	let eye = eyes[Math.floor(Math.random() * eyes.length)];
+	let mouth;
+	do {
+		mouth = mouths[Math.floor(Math.random() * mouths.length)];
+	} while(mouth.toLowerCase() == eye.toLowerCase());
+	let extra = extras[Math.floor(Math.random() * extras.length)];
+	
+	return extra[0] + eye + mouth + eye + extra[1];
+}
+
 const splashes = [
 	"Hey .o/",
 	"Nintendo DSi!",
 	"ニンテンドーDSi!",
 	"닌텐도 DSi!",
-	"Anyone else still use ニンテンドーDSiサウンド?",
 	"30 Pin > Lightning",
 	"Kyocera GRATINA 4G! <small>Flip phones are still cool, right?</small>",
 	"ピーケー十一",
@@ -12,10 +30,12 @@ const splashes = [
 	"TWL-001 (KOR)をプレイ中",
 	"iPod nano 6 is the best smart watch!",
 	"メロエッタは私の好きなポケモンです",
-	"아이 캔 소르타 리드 한글... (디스 이스 인 엔글리쉬, 밭 노 구글 트란스랱 니덷!)",
 	"行くぜっ！怪盗少女 is a good song",
 	"Twemoji are the best emoji",
-	"Why does the ;P emoji have a white around the open eye <small>it creeps me out</small>"
-]
+	"Why does 😜 have a white around the open eye <small>it creeps me out</small>",
+	`You've been to this page ${localStorage.visitCount} time${localStorage.visitCount == 1 ? "" : "s"}!`,
+	"<a href=\"javascript:/*quit_looking_at_the_link...thats_rude...*/!confirm('bwa ha ha! you are hackifyed!')?confirm('what do you mean no? thats rude :pout:')?null:window.open('https://youtu.be/dQw4w9WgXcQ','_self', ''):document.documentElement.style.setProperty('--primary','#'+Math.floor(Math.random()*0xFFFFFF).toString(16));\">Click me</a> to get <s>hacked</s> free cool stuff!",
+	"<span onmouseover=\"event.target.innerText = face();\" onmouseout=\"event.target.innerText = 'UwU';\">UwU</span>"
+];
 
 document.getElementById("splash").innerHTML = splashes[Math.floor(Math.random() * splashes.length)];
