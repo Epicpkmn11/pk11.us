806446c7aae5ab0d0ea382fdf6b9f85c38156729
Pk11 <epicpkmn11@outlook.com>
2020-11-30 07:57:12 -0600

    Bit more padding

--- assets/js/splashes.js
+++ assets/js/splashes.js
@@ -61,7 +61,7 @@ const splashes = [
 	"<a href=\"javascript:/*quit_looking_at_the_link...thats_rude...*/!confirm('bwa ha ha! you are hackifyed!')?confirm('what do you mean no? thats rude :pout:')?null:window.open('https://youtu.be/dQw4w9WgXcQ','_self', ''):document.documentElement.style.setProperty('--primary','#'+Math.floor(Math.random()*0xFFFFFF).toString(16));\">Click me</a> to get <s>hacked</s> free cool stuff!",
 	"<span onmouseover=\"event.target.innerText = face();\" onmouseout=\"event.target.innerText = 'UwU';\">UwU</span>",
 	`Error ${Math.floor(Math.random() * 1000)}!`,
-	`Your fortune today is... <span style='background-color: var(--blockquote-color); border-radius: 3px; padding: 0 3px; cursor: pointer;' onclick='this.style.backgroundColor = "#99999940"; this.style.cursor = ""'>${fortunes[getIndex("fortune", fortunes.length)]}</span>`,
+	`Your fortune today is... <span style='background-color: var(--blockquote-color); border-radius: 3px; padding: 2px 3px; cursor: pointer;' onclick='this.style.backgroundColor = "#99999940"; this.style.cursor = ""'>${fortunes[getIndex("fortune", fortunes.length)]}</span>`,
 ];
 
 document.getElementById("splash").innerHTML = splashes[getIndex("splash", splashes.length)];
